1b15a3f1c8c4e2a82ba7e5e4c2fb6c89ddde629f
jk7404 <jk7404@nyu.edu>
2025-08-14 05:55:30 +0000

    Another

--- repair_agent/initialization.sh
+++ repair_agent/initialization.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 sudo apt-get update 
 sudo apt-get install -y openjdk-11-jdk subversion dos2unix 
-cd defects4j 
+git clone https://github.com/rjust/defects4j
+cd defects4j
 sudo apt-get install -y cpanminus 
 cpanm --installdeps . 
 ./init.sh
 sudo apt-get install -y libdbi-perl
 cd ..
 python3 -m pip install -r requirements.txt
+python3 get_defects4j_list.py
